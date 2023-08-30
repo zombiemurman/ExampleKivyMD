e5fc4ea366e284eba72825b9a4c8dbdf404aba29
OMFB <zombiemurmna@yandex.ru>
2023-08-30 21:23:54 +0300

    Разработка меню

--- main.py
+++ main.py
@@ -1,10 +1,78 @@
+from kivy.lang import Builder
+from kivy.properties import ObjectProperty
+
 from kivymd.app import MDApp
-from kivymd.uix.label import MDLabel
+from kivymd.uix.scrollview import MDScrollView
+
+KV = '''
+<ContentNavigationDrawer>
+
+    MDList:
+
+        OneLineListItem:
+            text: "Screen 1"
+            on_press:
+                root.nav_drawer.set_state("close")
+                root.screen_manager.current = "scr 1"
+
+        OneLineListItem:
+            text: "Screen 2"
+            on_press:
+                root.nav_drawer.set_state("close")
+                root.screen_manager.current = "scr 2"
 
 
-class MainApp(MDApp):
+MDScreen:
+
+    MDTopAppBar:
+        pos_hint: {"top": 1}
+        elevation: 4
+        title: "Mortgage Calculator"
+        left_action_items: [["menu", lambda x: nav_drawer.set_state("open")]]
+
+    MDNavigationLayout:
+
+        MDScreenManager:
+            id: screen_manager
+
+            MDScreen:
+                name: "scr 1"
+
+                MDLabel:
+                    text: "Screen 1"
+                    halign: "center"
+                    
+                MDTextField:
+                    hint_text: "No helper text" 
+                    halign: "center"   
+
+            MDScreen:
+                name: "scr 2"
+
+                MDLabel:
+                    text: "Screen 2"
+                    halign: "center"
+
+        MDNavigationDrawer:
+            id: nav_drawer
+            radius: (0, 16, 16, 0)
+
+            ContentNavigationDrawer:
+                screen_manager: screen_manager
+                nav_drawer: nav_drawer
+'''
+
+
+class ContentNavigationDrawer(MDScrollView):
+    screen_manager = ObjectProperty()
+    nav_drawer = ObjectProperty()
+
+
+class MortgageCalculatorApp(MDApp):
     def build(self):
-        return MDLabel(text="Hello, World", halign="center")
+        self.theme_cls.primary_palette = "Orange"
+        self.theme_cls.theme_style = "Dark"
+        return Builder.load_string(KV)
 
 
-MainApp().run()
+MortgageCalculatorApp().run()
